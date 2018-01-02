fd8610fe265e9db062e15f0280f7545582cca093
Maciej Bogusz <maciekbogusz@yahoo.com>
2018-01-02 23:15:32 +0100

    Add files via upload

--- kalkulator.py
+++ kalkulator.py
@@ -1,22 +1,22 @@
 def calculator(operand1, operand2):
-    if operator == "+":
-        result = operand1+operand2
-    elif operator == "-":
-        result = operand1-operand2
-    elif operator == "*":
-        result = operand1*operand2
-    elif operator == "/":
-        if float_operand2 > 0: 
-            result = operand1/operand2
-        else:
-            print("Can`t divide by zero")
-    return result
+      if operator == "+":
+            result = operand1+operand2
+      elif operator == "-":
+            result = operand1-operand2
+      elif operator == "*":
+            result = operand1*operand2
+      elif operator == "/":
+            if float_operand2 > 0: 
+                  result = operand1/operand2
+            else:
+                  print("Can`t divide by zero")
+      return result
             
 operators = ["+", "-", "*", "/"]         
-condition = "Yes"
-result1 = 0
+temp_condition = condition = "YES"
+temp_result = 0
 
-while condition == "Yes":
+while temp_condition.upper() == condition:
       operand1 = input("Please enter number: ")
       operator = input("Please enter operator: ")
       operand2 = input("Please enter number: ")    
@@ -26,10 +26,10 @@ while condition == "Yes":
             for elenent in operators:
                   if operator == elenent:
                         result = calculator(float_operand1, float_operand2) 
-                        result1 = result +  result1
-                        print(result1)
-                        condition = input("Shall we continue? ")     
+                        temp_result = result +  temp_result
+                        print(temp_result)
+                        temp_condition = input("Shall we continue? ")     
       else:
             print("Wrong parameters provided")
 else:
-      print(result1)
+      print(temp_result)

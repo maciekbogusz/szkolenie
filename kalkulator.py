45d7b7cac4a12f6493acad8e0cd1ce700cdcfdc2
Maciej Bogusz <maciekbogusz@yahoo.com>
2018-01-02 18:53:50 +0100

    Add files via upload

--- kalkulator.py
+++ kalkulator.py
@@ -1,36 +1,35 @@
 def calculator(operand1, operand2):
     if operator == "+":
         result = operand1+operand2
-        print(result)
     elif operator == "-":
-        result = operand1+operand2
-        print(result)
+        result = operand1-operand2
     elif operator == "*":
         result = operand1*operand2
-        print(result)
     elif operator == "/":
         if float_operand2 > 0: 
             result = operand1/operand2
-            print(result)
         else:
             print("Can`t divide by zero")
+    return result
+            
+operators = ["+", "-", "*", "/"]         
+condition = "Yes"
+result1 = 0
 
-# def check_operands(number1):
-#     if type(number1) == int or type(number1) == float:
-#         number = float(number1)
-#         return number
-#             
-operand1 = input("Please enter number: ")
-operator = input("Please enter operator: ")
-operand2 = input("Please enter number: ")
-
-float_operand1 = float(operand1)
-float_operand2 = float(operand2)
-
-calculator(float_operand1, float_operand2)
-
-cont = input("Shall we continue? ")
-if cont == "Yes":
-    calculator(operand1, operand2)
-elif cont == "No":
-    print("Thank you ")
+while condition == "Yes":
+      operand1 = input("Please enter number: ")
+      operator = input("Please enter operator: ")
+      operand2 = input("Please enter number: ")    
+      if operand1.isnumeric() == True & operand2.isnumeric() == True:
+            float_operand1 = float(operand1)
+            float_operand2 = float(operand2)
+            for elenent in operators:
+                  if operator == elenent:
+                        result = calculator(float_operand1, float_operand2) 
+                        result1 = result +  result1
+                        print(result1)
+                        condition = input("Shall we continue? ")     
+      else:
+            print("Wrong parameters provided")
+else:
+      print(result1)

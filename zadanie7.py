b9cb540c29ad0b03cc31dc1bd12e8e4a1830964f
Maciej Bogusz <maciekbogusz@yahoo.com>
2018-01-11 15:31:01 +0100

    new task

---
+++ zadanie7.py
@@ -0,0 +1,12 @@
+# Merge two dictionaries (create new one) by following these rules:
+# 
+#     if some key exist only in one dictionary, then merged dictionary will have list of single item as a value
+#     if key exist in both of them, then merged dictionary should have list of two values
+# 
+# Ex:
+
+dict1 = {'6XM': 0.03656, 'X9S': 0.5296, '40M': 0.4625, '5ZI': 0.1843, 'BY8': 0.2344, 'B96': 0.902}
+dict2 = {'SBM': 0.7874, 'EC1': 0.9828, 'SDZ': 0.7338, '8XY': 0.885, 'X9S': 0.4425, '40M': 0.581}
+mergedDict = {}
+
+#Your code goes here
